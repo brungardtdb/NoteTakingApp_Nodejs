a41980fcfba3d40e0252ea357001a267dbfdcfe0
David Brungardt <brungardtdb@gmail.com>
2020-10-27 16:03:33 -0600

    Challenge activity, passed a function from another file into main applicatin. Decided to use towers of hanoi just for fun.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const addFunc = require('./utils.js')
+// const addFunc = require('./utils.js')
 
-const sum = addFunc(4,-2)
+// const sum = addFunc(4,-2)
 
-console.log(sum)
+// console.log(sum)
+
+const towers = require('./utils.js')
+
+const moveDiscs = towers(4, 'A', 'B', 'C')
--- utils.js
+++ utils.js
@@ -1,10 +1,24 @@
-console.log('utils.js')
+// console.log('utils.js')
 
-const name = 'David Brungardt'
+// const name = 'David Brungardt'
 
-const add = function (m, n) {
-    return m + n
+// const add = function (m, n) {
+//     return m + n
+// }
+
+// // define things to share with other files
+// module.exports = add
+
+const TowersOfHanoi = function (numDiscs, start, aux, finish) {
+
+    if (numDiscs == 1) {
+        console.log(start + '=>' + finish)
+    } else {
+        TowersOfHanoi(numDiscs - 1, start, finish, aux)
+        console.log(start + '=>' + finish)
+        TowersOfHanoi(numDiscs - 1, aux, start, finish)
+    }
+
 }
 
-// define things to share with other files
-module.exports = add
+module.exports = TowersOfHanoi
