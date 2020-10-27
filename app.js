553f445fd4a0228f60e5f078b5d7c7d12bcfbb35
David Brungardt <brungardtdb@gmail.com>
2020-10-27 17:02:45 -0600

    Added chalk to project.

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 // const addFunc = require('./utils.js')
+const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 const message = getNotes()
 console.log(message)
+
+console.log(validator.isEmail('blarp@gmail.com'))
+console.log(chalk.blue('Hello Blue World!'))
