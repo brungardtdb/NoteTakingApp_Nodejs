aaade2e7c26d38e757b013fd2a0101c68fb05d9c
David Brungardt <brungardtdb@gmail.com>
2020-10-27 13:51:16 -0600

    First challenge, challenge was to append text to file. I anticipate these will become more difficult as I move through this course.

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const fs = require('fs');
 
-fs.writeFileSync('notes.txt', 'This file was created by Node.js!');
+fs.writeFileSync('notes.txt', 'My name is David.');
+
+/*
+Challenge: Append a message to notes.txt.
+
+1. Use appendFileSync to append to the file.
+2. Run the script
+3. Check your work by opening notes.txt and verifying appended text is there.
+*/
+
+fs.appendFileSync('notes.txt', '\nI am having fun learning node.js!')
