e1dc438b62657d94dbc008ed5040e108b28d39ec
David Brungardt <brungardtdb@gmail.com>
2020-10-27 15:47:10 -0600

    Added utils.js file to begin to learn how to work with other files using node.js.

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-const fs = require('fs');
+utils = require('./utils.js')
 
-fs.writeFileSync('notes.txt', 'My name is David.');
+const name = 'David'
 
-/*
-Challenge: Append a message to notes.txt.
-
-1. Use appendFileSync to append to the file.
-2. Run the script
-3. Check your work by opening notes.txt and verifying appended text is there.
-*/
-
-fs.appendFileSync('notes.txt', '\nI am having fun learning node.js!')
+console.log(name)
