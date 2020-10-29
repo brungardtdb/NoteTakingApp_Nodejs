35345b20946e2e831cfd64d9125038f70ad7f0bc
David Brungardt <brungardtdb@gmail.com>
2020-10-28 19:45:16 -0600

    Setup outline for yargs commands for our note taking console application.

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
 const chalk = require('chalk')
 const getNotes = require('./notes.js')
+const yargs = require('yargs')
 
-const command = process.argv[2]
+// Customize yargs version
+yargs.version('1.1.0')
 
-if (command === 'add') {
-    console.log('Adding note')
-}
+// Create add command 
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note.',
+    handler: function() {
+        console.log('Adding a new note.')
+    }
+})
 
-if (command === 'remove') {
-    console.log('Removing note')
-}
+// Create remove command
+yargs.command({
+    command: 'remove', 
+    describe: 'Remove a note.', 
+    handler: function() {
+        console.log('Removing a note.')
+    }
+
+})
+
+// Setup list command
+yargs.command({
+    command: 'list',
+    describe: 'List notes.',
+    handler: function() {
+        console.log('Listing notes.')
+    }
+})
+
+// Setup read command
+yargs.command({
+    command: 'read',
+    describe: 'Reading note(s).',
+    handler: function(){
+        console.log('Reading note(s).')
+    }
+})
+
+// add, remove, read, list
+
+console.log(yargs.argv)
