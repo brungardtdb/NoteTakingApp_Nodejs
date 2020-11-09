2746040613be7af99c0c79bac027263f103798e9
David Brungardt <brungardtdb@gmail.com>
2020-11-08 18:44:45 -0700

    Added some exception handling.

--- notes.js
+++ notes.js
@@ -18,13 +18,25 @@ const addNote = function (title, body) {
     const noteString = JSON.stringify(note)
 
     if (!(notes === null)) {
-        // If file exists, append to file
-        console.log("Appending to file")
-        fs.appendFileSync('notes.json', noteString)
+
+        try {
+            // If file exists, append to file
+            console.log("Appending to file")
+            fs.appendFileSync('notes.json', noteString)
+        } catch (error) {
+            console.log(error)
+        }
+        
     }
     else { // If notes is null, file has not been created
-        console.log("Writing file")
-        fs.writeFileSync('notes.json', noteString)
+        
+        try {
+            console.log("Writing file")
+            fs.writeFileSync('notes.json', noteString)
+        } catch (error) {
+            console.log(error)
+        }
+        
     }
 
 }
@@ -34,13 +46,17 @@ const loadNotes = function() {
     // Check to see if file for notes exists
     if (fs.existsSync('notes.json')) {
 
-        // Return JSON data if file exists
-        // Data must be converted from buffer, to string, to JSON object
-        const dataBuffer = fs.readFileSync('notes.json')
-        const dataJSON = dataBuffer.toString()
-        const data = JSON.parse(dataJSON)
-        return dataJSON
-        
+        try {
+            // Return JSON data if file exists
+            // Data must be converted from buffer, to string, to JSON object
+            const dataBuffer = fs.readFileSync('notes.json')
+            const dataJSON = dataBuffer.toString()
+            const data = JSON.parse(dataJSON)
+            return dataJSON            
+        } catch (error) {
+            console.log(error)
+        }        
+
     } else {
         // Return null if file doesn't exist
         return null
